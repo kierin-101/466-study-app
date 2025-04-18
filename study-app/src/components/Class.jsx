5ae2d9ccb4c00dd5149f037d5fe022c664f22584
Kierin <kandrews4@huskers.unl.edu>
2025-04-18 14:23:03 -0500

    added new quiz button

--- study-app/src/components/Class.jsx
+++ study-app/src/components/Class.jsx
@@ -53,26 +53,26 @@ const Class = () => {
 
   const ClassSidebar = () => {
     return (
-        <div style={{height: "100%", width: "20vw", maxWidth: "20vw", background: "lightgrey", position: "fixed"}}>
-            <h1 style={{textAlign: "center"}}>Class Name</h1>
-            {showTeacherView && <p style={{marginLeft:"8px"}}><b>Join Code: </b>Code</p>}
-            <p style={{marginLeft:"8px"}}><b>Subject: </b>Class Subject</p>
-            <p style={{marginLeft:"8px"}}><b>Teacher: </b>Teacher</p>
-            <p style={{marginLeft:"8px"}}><b>Points Daily: </b>Points</p> {/* would be good to show user's progress toward the cap for the day */}
-        </div>
+      <div style={{ height: "100%", width: "20vw", maxWidth: "20vw", background: "lightgrey", position: "fixed" }}>
+        <h1 style={{ textAlign: "center" }}>Class Name</h1>
+        {showTeacherView && <p style={{ marginLeft: "8px" }}><b>Join Code: </b>Code</p>}
+        <p style={{ marginLeft: "8px" }}><b>Subject: </b>Class Subject</p>
+        <p style={{ marginLeft: "8px" }}><b>Teacher: </b>Teacher</p>
+        <p style={{ marginLeft: "8px" }}><b>Points Daily: </b>Points</p> {/* would be good to show user's progress toward the cap for the day */}
+      </div>
     )
   }
 
   const displayPerson = (personDetails, index) => {
     return (
-      <div key={`person${personDetails.personId}`} style={{width: "auto", display: "flex", alignItems:"center", justifyContent: "space-between", gap: "16px", padding: "8px", border: "8px solid lightgrey", borderRadius: "8px"}}>
-        <div className="personDetails" style={{width: "80%", display: "flex", alignItems:"center", justifyContent: "spaceBetween", gap: "16px"}}>
+      <div key={`person${personDetails.personId}`} style={{ width: "auto", display: "flex", alignItems: "center", justifyContent: "space-between", gap: "16px", padding: "8px", border: "8px solid lightgrey", borderRadius: "8px" }}>
+        <div className="personDetails" style={{ width: "80%", display: "flex", alignItems: "center", justifyContent: "spaceBetween", gap: "16px" }}>
           <AvatarDisplay dimension="64px" avatarName="test" borderName="test" />
-          <h3 style={{width: "25%"}}>{personDetails.username}</h3>
-          <p style={{width: "25%"}}>Title here</p>
-          <p style={{width: "25%"}}>{personDetails.isTeacher ? "Teacher" : "Student"}</p>
+          <h3 style={{ width: "25%" }}>{personDetails.username}</h3>
+          <p style={{ width: "25%" }}>Title here</p>
+          <p style={{ width: "25%" }}>{personDetails.isTeacher ? "Teacher" : "Student"}</p>
         </div>
-        {(!personDetails.isTeacher && showTeacherView) && <button onClick={() => {removeMember(personDetails, index);}}>Remove User</button>}
+        {(!personDetails.isTeacher && showTeacherView) && <button onClick={() => { removeMember(personDetails, index); }}>Remove User</button>}
       </div>
     )
   }
@@ -88,22 +88,22 @@ const Class = () => {
 
   const displayQuiz = (quizDetails) => {
     return (
-      <div key={`quiz${quizDetails.quizId}`} style={{width: "auto", display: "flex", alignItems:"center", justifyContent: "start", gap: "16px", padding: "8px", border: "8px solid lightgrey", borderRadius: "8px"}}>
-        <a href='./takeQuiz' className="quizLink" style={{width: "100%", display: "flex", alignItems:"center", justifyContent: "center", gap: "16px"}}>
-          <h3 style={{width: "20%"}}>{quizDetails.quizName}</h3>
-          <p style={{width: "20%"}}><b>Released: </b>{quizDetails.releaseDate.toDateString()}</p>
-          <p style={{width: "20%"}}><b>Due: </b>{quizDetails.dueDate.toDateString()}</p>
-          <p style={{width: "20%"}}><b>Target Score: </b>{quizDetails.targetScore || "None"}</p>
-          <p style={{width: "20%"}}><b>My Record: </b>todo</p>
+      <div key={`quiz${quizDetails.quizId}`} style={{ width: "auto", display: "flex", alignItems: "center", justifyContent: "start", gap: "16px", padding: "8px", border: "8px solid lightgrey", borderRadius: "8px" }}>
+        <a href='./takeQuiz' className="quizLink" style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center", gap: "16px" }}>
+          <h3 style={{ width: "20%" }}>{quizDetails.quizName}</h3>
+          <p style={{ width: "20%" }}><b>Released: </b>{quizDetails.releaseDate.toDateString()}</p>
+          <p style={{ width: "20%" }}><b>Due: </b>{quizDetails.dueDate.toDateString()}</p>
+          <p style={{ width: "20%" }}><b>Target Score: </b>{quizDetails.targetScore || "None"}</p>
+          <p style={{ width: "20%" }}><b>My Record: </b>todo</p>
         </a>
       </div>
     )
   }
 
   return (
-    <div style={{display: "flex"}}>
+    <div style={{ display: "flex" }}>
       <ClassSidebar />
-      <div style={{width: "100vw", minHeight:"95vh", height:"auto", marginLeft: "20vw", marginBottom: "16px"}}>
+      <div style={{ width: "100vw", minHeight: "95vh", height: "auto", marginLeft: "20vw", marginBottom: "16px" }}>
         <Tabs>
           <TabList>
             <Tab>All Quizzes</Tab>
@@ -112,37 +112,46 @@ const Class = () => {
             <Tab>Past Quizzes</Tab>
             <Tab>People</Tab>
           </TabList>
-        <TabPanel>
-          <h2 style={{marginLeft: "16px"}}>All Quizzes</h2>
-          <div style={{display: "flex", flexDirection: "column", gap:"16px", margin: "0px 16px"}}>
-            {quizList.map((quiz) => displayQuiz(quiz))}
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <h2 style={{marginLeft: "16px"}}>Open Quizzes</h2>
-          <div style={{display: "flex", flexDirection: "column", gap:"16px", margin: "0px 16px"}}>
-            {quizList.filter((quiz) => {const now = new Date(); return quiz.dueDate > now && quiz.releaseDate <= now}).map((quiz) => displayQuiz(quiz))}
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <h2 style={{marginLeft: "16px"}}>Upcoming Quizzes</h2>
-          <div style={{display: "flex", flexDirection: "column", gap:"16px", margin: "0px 16px"}}>
-            {quizList.filter((quiz) => {const now = new Date(); return quiz.releaseDate >= now}).map((quiz) => displayQuiz(quiz))}
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <h2 style={{marginLeft: "16px"}}>Past Quizzes</h2>
-          <div style={{display: "flex", flexDirection: "column", gap:"16px", margin: "0px 16px"}}>
-            {quizList.filter((quiz) => {const now = new Date(); return quiz.dueDate < now}).map((quiz) => displayQuiz(quiz))}
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <h2 style={{marginLeft: "16px"}}>People</h2>
-          <div style={{display: "flex", flexDirection: "column", gap:"16px", margin: "0px 16px"}}>
-            {peopleList.map((person, index) => displayPerson(person, index))}
-          </div>
-        </TabPanel>
-      </Tabs>
+          <TabPanel>
+            <h2 style={{ marginLeft: "16px" }}>All Quizzes</h2>
+            <div style={{ display: "flex", flexDirection: "column", gap: "16px", margin: "0px 16px" }}>
+              <a
+                href={"/createQuiz?class=" + (window.location.search.slice(1))}
+                style={{
+                  width: "100%", display: "flex", alignItems: "center",
+                  justifyContent: "center", background: "lightgrey",
+                  borderRadius: "8px", textDecoration: "none", color: "black"
+                }}>
+                <h3 style={{ width: "20%" }}>Create New Quiz</h3>
+              </a>
+              {quizList.map((quiz) => displayQuiz(quiz))}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <h2 style={{ marginLeft: "16px" }}>Open Quizzes</h2>
+            <div style={{ display: "flex", flexDirection: "column", gap: "16px", margin: "0px 16px" }}>
+              {quizList.filter((quiz) => { const now = new Date(); return quiz.dueDate > now && quiz.releaseDate <= now }).map((quiz) => displayQuiz(quiz))}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <h2 style={{ marginLeft: "16px" }}>Upcoming Quizzes</h2>
+            <div style={{ display: "flex", flexDirection: "column", gap: "16px", margin: "0px 16px" }}>
+              {quizList.filter((quiz) => { const now = new Date(); return quiz.releaseDate >= now }).map((quiz) => displayQuiz(quiz))}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <h2 style={{ marginLeft: "16px" }}>Past Quizzes</h2>
+            <div style={{ display: "flex", flexDirection: "column", gap: "16px", margin: "0px 16px" }}>
+              {quizList.filter((quiz) => { const now = new Date(); return quiz.dueDate < now }).map((quiz) => displayQuiz(quiz))}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <h2 style={{ marginLeft: "16px" }}>People</h2>
+            <div style={{ display: "flex", flexDirection: "column", gap: "16px", margin: "0px 16px" }}>
+              {peopleList.map((person, index) => displayPerson(person, index))}
+            </div>
+          </TabPanel>
+        </Tabs>
       </div>
     </div>
   );
